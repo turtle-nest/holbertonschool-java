c18ce0eacbe6eb93cd2fa2404dcf7f4e4985c693
turtle-nest <n78693107@gmail.com>
2025-05-02 15:51:38 +0200

    1. Complete task

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,7 @@
+public class PrintIntegerVariable {
+    public static void main(String[] args)
+    {
+        int number = 1600;
+        System.out.printf("Pennsylvania Avenue, %d\n", number);
+    }
+}
