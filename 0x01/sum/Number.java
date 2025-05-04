fde2558ff75a0bf6dcd8c5236c26308dd77d1873
turtle-nest <n78693107@gmail.com>
2025-05-04 23:07:44 +0200

    3. Complete task with personal correction in Program.java

---
+++ 0x01/sum/Number.java
@@ -0,0 +1,5 @@
+public class Number {
+    public static int DoSum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ 0x01/sum/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.printf("%d\n", Number.DoSum(24, 32));
+        System.out.printf("%d\n", Number.DoSum(5, 0));
+        System.out.printf("%d\n", Number.DoSum(4, -2));
+    }
+}
