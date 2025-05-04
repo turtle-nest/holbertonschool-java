7c0e60db172ce8cd1efbce2a3b5c7a184c6fba36
turtle-nest <n78693107@gmail.com>
2025-05-04 22:36:02 +0200

    1.Complete task

---
+++ 0x01/even/EvenNumbers.java
@@ -0,0 +1,9 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        String output = "";
+        for (int i = 0; i < 98; i += 2) {
+            System.out.print(i + ", ");
+        }
+        System.out.println(98);
+    }
+}
