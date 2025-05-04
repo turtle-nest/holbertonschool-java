e203087185fbe1f64a00937a542be17b131f0035
turtle-nest <n78693107@gmail.com>
2025-05-04 17:04:44 +0200

    4.Complete task

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String text1 = "Holberton";
+        String text2 = "School";
+        String concat = text1 + " " + text2;
+        System.out.printf("Welcome to %s !%n", concat);
+    }
+}
