721a40f6847121c71a916a4289f16d0960dd2689
turtle-nest <n78693107@gmail.com>
2025-05-04 16:54:27 +0200

    3. Complete task

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,8 @@
+public class PrintStringVariable {
+    public static void main(String[] args) {
+        String text = "Holberton School";
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text.substring(0, 9));
+    }
+}
