56914a47713b0e2b3831618ed817c378cfcd93ce
turtle-nest <n78693107@gmail.com>
2025-05-04 22:14:18 +0200

    0.Complete task

---
+++ 0x01/README.md
@@ -0,0 +1,5 @@
+# Java - Structures, Decision, and Repetition
+0. Numbers 0 to 99
+1. Even numbers from 0 to 99
+2. Uppercase Letter
+3. Sum
---
+++ 0x01/numbers/Numbers0to99.java
@@ -0,0 +1,8 @@
+public class Numbers0to99 {
+    public static void main(String[] args) {
+        for (int num = 0; num < 99; num++) {
+            System.out.printf("%d, ", num);
+        }
+        System.out.println(99);
+    }
+}
