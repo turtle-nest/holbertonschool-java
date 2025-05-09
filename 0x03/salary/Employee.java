e6e05b556e1fb9d8974b587a7cf6f6888d753cd8
turtle-nest <n78693107@gmail.com>
2025-05-09 14:14:50 +0200

    0. Fix the Employee class with Math.ceil

--- 0x03/salary/Employee.java
+++ 0x03/salary/Employee.java
@@ -11,7 +11,7 @@ public class Employee {
 
     public double calculateBonus(Department department) {
         if (department.reachedTarget()) {
-            return fixedSalary * 0.10;
+            return Math.ceil(fixedSalary * 0.10);
         } else {
             return 0.00;
         }
